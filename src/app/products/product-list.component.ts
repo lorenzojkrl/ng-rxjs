b2887c95e346db48e8d8ca079b2742784a9bdcd6
Lorenzo <lorenzozar@gmail.com>
2022-02-08 22:12:02 +0200

    pipe async to display observable in template

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,30 +1,26 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products: Product[] = [];
+  products$: Observable<Product[]>;
   sub: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe(
-        products => this.products = products,
-        error => this.errorMessage = error
-      );
+    this.products$ = this.productService.getProducts();
   }
 
   ngOnDestroy(): void {
